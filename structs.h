308cc61c34aae2799261cd1cd088390475243561
tehtnaz <jasebeaumont@gmail.com>
2025-10-31 16:59:11 -0400

    Forgot to add structs.h

---
+++ structs.h
@@ -0,0 +1,22 @@
+#ifndef PFE_STRUCTS
+
+// Struct for combining accepted flex ranges for each letter
+struct LetterFlexRange{
+  int flexMin1;
+  int flexMax1;
+
+  int flexMin2;
+  int flexMax2;
+
+  int flexMin3;
+  int flexMax3;
+
+  int flexMin4;
+  int flexMax4;
+
+  int flexMin5;
+  int flexMax5;
+};
+#define PFE_STRUCTS
+
+#endif
